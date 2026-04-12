0bbc357fa79014202dd5d42b6cf18226244e15b6
kemal <karul@tuta.io>
2026-04-12 17:40:09 +0300

    bullshit

--- main.user.js
+++ main.user.js
@@ -3,14 +3,15 @@
 // @namespace    dumbovita
 // @homepage     https://github.com/dumbovita/bravesearch-noai
 // @author       dumbovita
-// @version      0.1
+// @version      0.2
 // @description  hides ai features in brave search
 // @match        https://search.brave.com/*
 // @grant        none
+// @run-at       document-start
 // ==/UserScript==
 
-document.head.appendChild(
+(document.head || document.documentElement).appendChild(
     Object.assign(document.createElement("style"), {
-        textContent: `#submit-llm-button,#ai-features-button,#chatllm-main-answer-content,#llm-snippet,#chatllm-conversation,.chatllm-content,.chatllm-answer-list,.chatllm-disclaimer,.tab-item a[href*="/ask"],.llm-output,.followups-wrapper,#chatllm-actions,.qa-inline-entity,.inline-refs,[class*="llm-"]{display:none!important;}`,
+        textContent: `#submit-llm-button,#ai-features-button,#chatllm-main-answer-content,#llm-snippet,#chatllm-conversation,.chatllm-content,.chatllm-answer-list,.chatllm-disclaimer,.tab-item a[href*="/ask"],.llm-output,.followups-wrapper,#chatllm-actions,.qa-inline-entity,.inline-refs,[class*="llm-"],.download-cta{display:none!important;}`,
     }),
 );
